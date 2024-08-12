18619d9e57d9635936e0f59399d9983f62e578d5
sanetrox94 <doom102010.ccsa@gmail.com>
2024-08-12 07:10:40 -0500

    Fix test

--- src/test/java/sanetroxdev/crud/invoice/services/InvoiceServicesTests.java
+++ src/test/java/sanetroxdev/crud/invoice/services/InvoiceServicesTests.java
@@ -7,11 +7,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.mockito.ArgumentMatchers.any;
+
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.mockito.junit.jupiter.MockitoExtension;
-import sanetroxdev.crud.invoice.dtos.ContactDto;
 import sanetroxdev.crud.invoice.dtos.InvoiceDto;
 import sanetroxdev.crud.invoice.dtos.InvoiceMapper;
 import sanetroxdev.crud.invoice.models.ContactEntity;
@@ -60,43 +59,41 @@ class InvoiceServicesTests {
                 .contacts(List.of(contact))
                 .build();
 
-        // Configura el comportamiento del repositorio para devolver la entidad existente
-        given(invoiceRepository.findByReferenceOne("123")).willReturn(Optional.of(invoice));
-        // Configura el comportamiento del repositorio para guardar la entidad
-        given(invoiceRepository.save(invoice)).willReturn(invoice);
 
-        // Configura el comportamiento del servicio de contacto
-        given(contactService.updateContact(any(ContactDto.class))).willReturn(contact);
     }
 
-//    @DisplayName("Test para Listar Invoice")
-//    @Test
-//    void testparaListarInvoice(){
-//
-//
-//        //Given
-//        InvoiceEntity invoice2 = InvoiceEntity.builder()
-//                .id(1L)
-//                .amount(BigDecimal.valueOf(100))
-//                .referenceOne("ref-2")
-//                .date(LocalDate.now())
-//                .build();
-//
-//        given(invoiceRepository.findAll()).willReturn(List.of(invoice, invoice2));
-//
-//        //When
-//        List<InvoiceDto> invoices = invoiceService.getinvoices();
-//
-//        //Then
-//        assertThat(invoices).isNotNull().hasSize(2);
-//
-//    }
+    @DisplayName("Test para Listar Invoice")
+    @Test
+    void testparaListarInvoice(){
+
+
+        //Given
+        InvoiceEntity invoice2 = InvoiceEntity.builder()
+                .id(1L)
+                .amount(BigDecimal.valueOf(100))
+                .referenceOne("ref-2")
+                .date(LocalDate.now())
+                .build();
+
+        given(invoiceRepository.findAll()).willReturn(List.of(invoice, invoice2));
+
+        //When
+        List<InvoiceDto> invoices = invoiceService.getinvoices();
+
+        //Then
+        assertThat(invoices).isNotNull().hasSize(2);
+
+    }
 
     @DisplayName("Test para Actualizar Invoice")
     @Test
     void testparaActualizarInvoice(){
 
         //Given
+        given(invoiceRepository.save(invoice)).willReturn(invoice);
+        // Configura el comportamiento del repositorio para devolver la entidad existente
+        given(invoiceRepository.findByReferenceOne("123")).willReturn(Optional.of(invoice));
+        // Configura el comportamiento del repositorio para guardar la entidad
         given(invoiceRepository.save(invoice)).willReturn(invoice);
         BigDecimal newAmount = BigDecimal.valueOf(200);
         invoice.setAmount(newAmount);
